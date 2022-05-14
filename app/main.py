842f36b582e47ae7e76f1a0c76723f9651a7d6f8
Anton Horodchuk <antoman1994@gmail.com>
2022-05-14 10:40:05 +0300

    Added a comment with expected and actual behaviour

--- app/main.py
+++ app/main.py
@@ -1,4 +1,9 @@
 
 
 if __name__ == '__main__':
+    # Mark the 'app' directory as Sources Root
+    # Try to auto-import the class HelloWorldPrinter.
+    #   You will get 'lib.hello.HelloWorldPrinter' instead of
+    #       'app.lib.hello.HelloWorldPrinter'
     printer = HelloWorldPrinter()
+    printer.print_hello_world()
